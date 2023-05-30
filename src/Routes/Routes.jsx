23a37b963d1879b5e255391b30bc1b5607fb7b40
Mousumi-Akter72 <mousumi.akter1072@gmail.com>
2023-05-30 13:21:37 +0600

    done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,30 +1,45 @@
 import {
-    createBrowserRouter,
-   
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import Main from "../Layout/Main";
-import Home from "../Pages/Home/Home/Home";
-import Menu from "../Pages/Menu/Menu/Menu";
-import Order from "../Pages/Order/Order/Order";
+import Home from "../pages/Home/Home/Home";
+import Menu from "../pages/Menu/Menu/Menu";
+import Order from "../pages/Order/Order/Order";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
+import PrivateRoute from "./PrivateRoute";
+import Secret from "../pages/Shared/Secret/Secret";
 
 
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-       {
-        path: "/",
-      element: <Home></Home>,
-       },
-       {
-        path: "menu",
-      element: <Menu></Menu>,
-       },
-       {
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+          path: '/',
+          element: <Home></Home>
+      }, 
+      {
+        path: 'menu', 
+        element: <Menu></Menu>
+      },
+      {
         path: 'order/:category',
         element: <Order></Order>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'signup',
+        element: <SignUp></SignUp>
+      },
+      {
+        path: 'secret',
+        element: <PrivateRoute><Secret></Secret></PrivateRoute>
       }
-      ]
-    },
-  ]);
+    ]
+  },
+]);
